10d3db2614c12d3662cd82172b9e7fbadf22e9ef
LengNan777 <L2452520160@gmail.com>
2022-10-19 18:01:18 -0500

    Add project files.

---
+++ Models/ParkingHelper.cs
@@ -0,0 +1,65 @@
+﻿using SD_125_W22SD_Lab2.Data;
+
+namespace SD_125_W22SD_Lab2.Models
+{
+    public class ParkingHelper
+
+    {
+
+        private ParkingContext parkingContext;
+
+        public ParkingHelper(ParkingContext context)
+
+        {
+
+            this.parkingContext = context;
+
+        }
+
+        public Pass CreatePass(string purchaser, bool premium, int capacity)
+
+        {
+            try
+            {
+                if(purchaser.Length<3 || purchaser.Length>20 || capacity <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                Pass newPass = new Pass();
+
+                newPass.Purchaser = purchaser;
+
+                newPass.Premium = premium;
+
+                newPass.Capacity = capacity;
+
+                parkingContext.Passes.Add(newPass);
+
+                parkingContext.SaveChanges();
+
+                return newPass;
+            }
+            catch(Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+            return null;
+        }
+
+        public ParkingSpot CreateParkingSpot()
+
+        {
+
+            ParkingSpot newSpot = new ParkingSpot();
+
+            newSpot.Occupied = false;
+
+            parkingContext.ParkingSpots.Add(newSpot);
+
+            return newSpot;
+
+        }
+
+    }
+}
